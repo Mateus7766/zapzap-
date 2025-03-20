141b85f2a54825f6ce78cd5d9fd21e26ebd0ca6e
Mateus Mendes <bonitoclash609@gmail.com>
2025-03-20 00:21:30 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WhatsApp Extension for Firefox
+
+Extensão que eu fiz para mostrar as mensagens apagadas do Whatsapp Web, já que eu não achei nenhuma nos addons do Firefox. Lembrando que isso pode parar de funcionar a qualquer momento e é totalmente feito na gambiarra já que foi feito para uso pessoal
---
+++ whatsapp.mjs
@@ -0,0 +1,32 @@
+const chatContainer = document.getElementById('app')
+
+const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+        mutation.removedNodes.forEach((node) => {
+            if (node.nodeType === Node.ELEMENT_NODE) {
+                if (node.getAttribute('tabindex') == "-1" && node.getAttribute('role') == "row" && node.getAttribute('class') == "") {
+                    if (!document.contains(node)) {
+                        const newNode = node.cloneNode(true);
+
+
+                        if (mutation.previousSibling) {
+                            mutation.previousSibling.after(newNode);
+                        } else if (mutation.nextSibling) {
+                            mutation.nextSibling.before(newNode);
+                        } else {
+                            mutation.target.appendChild(newNode);
+                        }
+
+                        const messageNode = newNode.firstChild.firstChild.childNodes[1]
+                        // messageNode.innerHTML = "__Mensagem apagada__";
+                        messageNode.style.color = "red";
+                        console.log(' cebola ', messageNode)
+
+                        // console.log("Mensagem foi excluída:", newNode);
+                    }
+                }
+            }
+        });
+    });
+});
+observer.observe(chatContainer, { childList: true, subtree: true });
